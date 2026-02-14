570de593466e0a4fc644f5563b23c5dcbbd60716
Sascco <sergiosolanoc@gmail.com>
2026-02-14 00:30:20 -0500

    Cleaning the repo

--- test_driver.py
+++
@@ -1,7 +0,0 @@
-# En test_driver.py
-from src.core.driver_factory import DriverFactory
-
-driver = DriverFactory.get_driver()
-driver.get("https://opensource-demo.orangehrmlive.com/")
-input("Presiona Enter para cerrar el navegador...")
-driver.quit()
